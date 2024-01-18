b059744e0be7948b5d71241261a813108bb7c2fc
Arshad Safi <ah.safi2001@gmail.com>
2024-01-18 17:03:20 -0500

    all functions added

--- Player.py
+++ Player.py
@@ -74,7 +74,7 @@ class GeniusComPlayer(Player):
         if state.current_winner == other_player:
             #it is best to return both position and score for our algorithm (minimax) to work
             return {"Position" : None,
-                    "score" : 1 * (state.num_empty_square() + 1) if other_player == max_player else -1 * (
+                    "Score" : 1 * (state.num_empty_square() + 1) if other_player == max_player else -1 * (
                         state.nu + 1)
                         }
         elif not state.empty_squares(): #no empty square
@@ -91,13 +91,21 @@ class GeniusComPlayer(Player):
 
             #a) make a move, and try that spot
             state.make_move(possible_move, player)
-
             #b) recurse using minimax to simulate a game after making that move
+            sim_score = self.minimax(state, other_player)#alternating Players here
 
             #c) unde the move
-
-
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score["Position"] = possible_move
             #d) update the the dicts if needed
+            if player == max_player:
+                if sim_score["Score"] > best["Score"]:
+                    best = sim_score
+            else:
+                if sim_score["Score"] < best["Score"]:
+                    best = sim_score
+        return best
 
 
 
